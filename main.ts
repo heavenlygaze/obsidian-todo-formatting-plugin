ef8d9d85de33fa98e8b6412f30f18d1a096b5e43
Nuraly <nuralyhug@gmail.com>
2025-04-21 01:13:41 +0500

    initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,139 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// CodeMirror 6 view API:
+import {
+  EditorView,
+  ViewPlugin,
+  ViewUpdate,
+  Decoration,
+  DecorationSet
+} from '@codemirror/view';
 
-// Remember to rename these classes and interfaces!
+// CodeMirror 6 state API:
+import { RangeSetBuilder } from '@codemirror/state';
 
-interface MyPluginSettings {
-	mySetting: string;
+interface TodoHighlightSettings {
+  todoColor: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: TodoHighlightSettings = {
+  todoColor: '#00FF00'
+};
+
+export default class TodoHighlightPlugin extends Plugin {
+  settings: TodoHighlightSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    // Inject CSS based on current setting
+    this.injectTodoStyle();
+
+    // Add our settings tab
+    this.addSettingTab(new TodoHighlightSettingTab(this.app, this));
+
+    // Preview/highlight in read mode
+    this.registerMarkdownPostProcessor((el) => {
+      const todoRx = /\bTODO\b/g;
+      el.querySelectorAll('p, li').forEach((node: HTMLElement) => {
+        node.innerHTML = node.innerHTML.replace(
+          todoRx,
+          `<span class="cm-todo">$&</span>`
+        );
+      });
+    });
+
+    // Highlight in edit mode 
+    const todoEditorDecorator = ViewPlugin.fromClass(
+      class {
+        decorations: DecorationSet;
+        constructor(view: EditorView) {
+          this.decorations = this.buildDeco(view);
+        }
+        update(update: ViewUpdate) {
+          if (update.docChanged || update.viewportChanged) {
+            this.decorations = this.buildDeco(update.view);
+          }
+        }
+        buildDeco(view: EditorView) {
+          const builder = new RangeSetBuilder<Decoration>();
+          const regex = /\bTODO\b/g;
+          for (const { from, to } of view.visibleRanges) {
+            const text = view.state.doc.sliceString(from, to);
+            let m;
+            while ((m = regex.exec(text)) !== null) {
+              const start = from + m.index;
+              const end = start + m[0].length;
+              builder.add(start, end, Decoration.mark({ class: 'cm-todo' }));
+            }
+          }
+          return builder.finish();
+        }
+      },
+      { decorations: v => v.decorations }
+    );
+    this.registerEditorExtension([todoEditorDecorator]);
+  }
+
+  onunload() {
+    this.removeTodoStyle();
+  }
+
+  // Load + save
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+  async saveSettings() {
+    await this.saveData(this.settings);
+    this.injectTodoStyle();
+  }
+
+  // Inject <style id="todo-highlight-style"> into document head
+  injectTodoStyle() {
+    const styleId = 'todo-highlight-style';
+    let styleTag = document.getElementById(styleId) as HTMLStyleElement;
+    if (!styleTag) {
+      styleTag = document.createElement('style');
+      styleTag.id = styleId;
+      document.head.appendChild(styleTag);
+    }
+    styleTag.textContent = `
+      .cm-todo {
+        color: ${this.settings.todoColor} !important;
+      }
+      /* also apply to preview spans just in case */
+      span.cm-todo {
+        color: ${this.settings.todoColor};
+      }
+    `;
+  }
+  removeTodoStyle() {
+    document.getElementById('todo-highlight-style')?.remove();
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// Settings tab to pick a colour
+class TodoHighlightSettingTab extends PluginSettingTab {
+  plugin: TodoHighlightPlugin;
+  constructor(app: App, plugin: TodoHighlightPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	async onload() {
-		await this.loadSettings();
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
+    containerEl.createEl('h2', { text: 'TODO Highlighter Settings' });
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('TODO color')
+      .setDesc('Choose the highlight colour for your TODOs.')
+      .addColorPicker(colorPicker =>
+        colorPicker
+          .setValue(this.plugin.settings.todoColor)
+          .onChange(async (value) => {
+            this.plugin.settings.todoColor = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
